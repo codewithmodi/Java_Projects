2447abb93b17ca5543b47e39c5b1effb19b40e9b
Ayush Modi <codewithmodi@gmail.com>
2020-02-24 00:03:19 +0530

    Second

--- j0002.java
+++ j0002.java
@@ -6,16 +6,21 @@ class j0002 {
         int score = 800;
         int bonus = 100;
         int level_completed = 5;
+        mainr(game_over, score, bonus, level_completed);
+        score = 10000;
+        bonus = 200;
+        level_completed = 9;
+        if (game_over) {
+            int second_score = score + (level_completed * bonus);
+            System.out.println("Your Second Final Score is this " + second_score);
+        }
+    }
+
+    public static void mainr(boolean game_over, int score, int bonus, int level_completed) {
         if (game_over) {
             int final_score = score + (level_completed * bonus);
             System.out.println("Your final Score is " + final_score);
         }
-        if (game_over) {
-            score = 10000;
-            bonus = 200;
-            level_completed = 8;
-            int second_score = score + (level_completed * bonus);
-            System.out.println("Your Second Final Score is this " + second_score);
-        }
+
     }
 }
